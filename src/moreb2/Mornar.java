fa7717aa3e1c79334690f528d841834bdc37232d
Korisnik <Korisnik@DESKTOP-6EEOAKT>
2022-10-13 15:29:31 +0200

    Mornar

---
+++ src/moreb2/Mornar.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moreb2;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class Mornar {
+    private String ime;
+    private int kvalitet;
+
+    public Mornar(String ime, int kvalitet) {
+        this.ime = ime;
+        this.kvalitet = kvalitet;
+    }
+
+    public String dohvatiIme() {
+        return ime;
+    }
+
+    public int dohvatiKvalitet() {
+        return kvalitet;
+        
+    }
+    
+    public String toString() {
+    return "M_" + ime + " :  " + kvalitet;
+    }
+    
+}
