a79e1d2d924eb2acea502b4926c68143206a1f7e
aurelgava <gava1977@gmail.com>
2022-10-13 13:36:55 +0200

    Flota

---
+++ src/moreb2/Flota.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package moreb2;
+
+/**
+ *
+ * @author aurel
+ */
+public class Flota {
+    private static class Elem {
+        Brod b; Elem sled;
+        Elem(Brod b) { this.b = b; }
+    }
+}
