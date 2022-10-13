4e231f3981a0f699ab87fb7927006ae9dab4c7e0
Ucenik <Ucenik@DESKTOP-DNCQMMT>
2022-10-13 15:54:17 +0200

    Klasa Flota zavrsena

--- src/moreb2/Flota.java
+++ src/moreb2/Flota.java
@@ -4,13 +4,51 @@
  */
 package moreb2;
 
-/**
- *
- * @author aurel
- */
-public class Flota {
+public class Flota{
     private static class Elem {
         Brod b; Elem sled;
-        Elem(Brod b) { this.b = b; }
+        //Elem(Brod b) { this.b = b; }
     }
+    
+    String naziv;
+    Elem brodovi = null;
+    char vrsta = '-'; // g - kraljevski, k - piratski
+    
+    public Flota(String s){
+        naziv = s;
+    }
+    
+    void dodaj(Brod brod) throws Exception{
+        if(brodovi == null){
+            vrsta = brod.dohvatiVrstu();
+            brodovi = new Elem();
+            brodovi.sled = null;
+        }
+        else{
+            if(brod.dohvatiVrstu() != vrsta)
+                throw new GNeadekvatan();           
+            
+            Elem e = brodovi;
+            while(e.sled != null){
+                e = e.sled;
+            }
+            e.b = brod;
+            e.sled = null;
+        }
+    }
+    Brod dohvati(int i) throws Exception{
+        Elem e = brodovi;
+        if(e == null)
+            throw new GIndeks();
+        while(i>=0){
+            i--;
+            if(e.sled == null)
+                break;
+            e = e.sled;
+        }
+        if(i != -1)
+            throw new GIndeks();
+        return e.b;
+    }
+    
 }
---
+++ src/moreb2/GNeadekvatan.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package moreb2;
+
+public class GNeadekvatan extends Exception{
+    public String toString(){
+        return "Neadekvatan brod";
+    }
+}
