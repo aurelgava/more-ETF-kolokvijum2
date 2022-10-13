152db5c0aa7f734e80d1e35b3047309f7c1e27a2
Ucenik <Ucenik@DESKTOP-DNCQMMT>
2022-10-13 16:26:45 +0200

    Zavrsene metode izmedju konstruktora i metode kvalitetPosade

--- src/moreb2/Brod.java
+++ src/moreb2/Brod.java
@@ -20,20 +20,45 @@ public abstract class Brod {
         mornari = new Mornar[brMornara];
         mornari[0] = kapetan;
         this.vrsta = vrsta;
+        trenutniBrMornara = 1;
     }
     public void dodajMorn(Mornar m) {
-        
+        mornari[trenutniBrMornara] = m;
+        if(mornari[trenutniBrMornara].dohvatiKvalitet() > mornari[0].dohvatiKvalitet()){
+            mornari[trenutniBrMornara] = mornari[0];
+            mornari[0] = m;
+        }
     }
+    
     public int dohvBr() {
-        
+        return brMornara;
     }
-    public Mornar dohvatiKapetana() {}
-    public int indeksNajgoregMornara(){}
-    public Mornar dohvatiNajboljegMornara(){}
-    public int dohvatiTrenutniBrMornara(){}
-    public char dohvatiVrstu(){}
-    public void ukloniSveMornare(){}
-    public double kvalitetPosade(){}
+    public Mornar dohvatiKapetana() {
+        return mornari[0];
+    }
+    public int indeksNajgoregMornara(){
+        int najgori = mornari[1].dohvatiKvalitet();
+        for(int i=2; i<trenutniBrMornara; i++){
+            if(mornari[i].dohvatiKvalitet() < najgori)
+                najgori = mornari[i].dohvatiKvalitet();
+        }
+        return najgori;
+    }
+    public Mornar dohvatiNajboljegMornara(){
+        return mornari[0];
+    }
+    public int dohvatiTrenutniBrMornara(){
+        return trenutniBrMornara;
+    }
+    public char dohvatiVrstu(){
+        return vrsta;
+    }
+    public void ukloniSveMornare(){
+        mornari = null;
+        trenutniBrMornara = 0;
+    }
+    public double kvalitetPosade(){
+    }
     public String toString() {}
     public abstract void napadni(Brod b);
     
